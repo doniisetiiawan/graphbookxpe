4023ca1861e025d3cb4d37dc00fd4ca197cde5d5
Doni <donisetiawan.050@gmail.com>
2019-04-10 17:28:20 +0700

    Add middleware

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,26 @@
 import express from 'express';
 import path from 'path';
+import helmet from 'helmet';
+import cors from 'cors';
+import compression from 'compression';
 
 const root = path.join(__dirname, '../../');
 
 const app = express();
 const port = 8000;
+
+app.use(helmet());
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'", "'unsafe-inline'"],
+    styleSrc: ["'self'", "'unsafe-inline'"],
+    imgSrc: ["'self'", 'data:', '*.amazonaws.com'],
+  },
+}));
+app.use(helmet.referrerPolicy({ policy: 'same-origin' }));
+app.use(compression());
+app.use(cors());
 
 app.use('/', express.static(path.join(root, 'dist/client')));
 app.use('/uploads', express.static(path.join(root, 'uploads')));
