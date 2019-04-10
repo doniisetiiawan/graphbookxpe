38a67d9249ed68ffeec4a509e295c0944cc14f08
Doni <donisetiawan.050@gmail.com>
2019-04-10 14:07:31 +0700

    Add event handling and state update

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -21,12 +21,44 @@ const posts = [{
 export default class App extends Component {
   state = {
     posts,
+    postContent: '',
+  };
+
+  handlePostContentChange = (event) => {
+    this.setState({ postContent: event.target.value });
+  };
+
+  handleSubmit = (event) => {
+    const { posts, postContent } = this.state;
+    event.preventDefault();
+    const newPost = {
+      id: posts.length + 1,
+      text: postContent,
+      user: {
+        avatar: '/uploads/avatar1.png',
+        username: 'Fake User',
+      },
+    };
+    this.setState(prevState => ({
+      posts: [newPost, ...prevState.posts],
+      postContent: '',
+    }));
   };
 
   render() {
-    const { posts } = this.state;
+    const { posts, postContent } = this.state;
     return (
       <div className="container">
+        <div className="postForm">
+          <form onSubmit={this.handleSubmit}>
+            <textarea
+              value={postContent}
+              onChange={this.handlePostContentChange}
+              placeholder="Write your custom post.."
+            />
+            <input type="submit" value="submit" />
+          </form>
+        </div>
         <div className="feed">
           {posts.map(post => (
             <div key={post.id} className="post">
