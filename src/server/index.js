941f305417d928a28aecb92be3976ecc78037d6a
Doni <donisetiawan.050@gmail.com>
2019-04-10 16:25:03 +0700

    node.js and express.js

---
+++ src/server/index.js
@@ -0,0 +1,7 @@
+import express from 'express';
+
+const app = express();
+const port = 8000;
+
+app.get('*', (req, res) => res.send('Hello world...'));
+app.listen(port, () => console.log('Listening on port 8000...'));
