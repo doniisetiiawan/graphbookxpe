37834137b90af56ed51535209ea08ae782c0ace2
Doni <donisetiawan.050@gmail.com>
2019-04-10 16:30:31 +0700

    Routing express.js

--- src/server/index.js
+++ src/server/index.js
@@ -3,5 +3,12 @@ import express from 'express';
 const app = express();
 const port = 8000;
 
-app.get('*', (req, res) => res.send('Hello world...'));
+app.get('/', (req, res, next) => {
+  console.log('First function...');
+  next();
+}, (req, res) => {
+  console.log('Second function...');
+  res.send('Hello world...');
+});
+
 app.listen(port, () => console.log('Listening on port 8000...'));
