5e48e6c5fbed69e10762223b0917dd6811319448
Doni <donisetiawan.050@gmail.com>
2019-04-10 16:51:40 +0700

    Serving production build

--- src/server/index.js
+++ src/server/index.js
@@ -1,14 +1,16 @@
 import express from 'express';
+import path from 'path';
+
+const root = path.join(__dirname, '../../');
 
 const app = express();
 const port = 8000;
 
-app.get('/', (req, res, next) => {
-  console.log('First function...');
-  next();
-}, (req, res) => {
-  console.log('Second function...');
-  res.send('Hello world...');
+app.use('/', express.static(path.join(root, 'dist/client')));
+app.use('/uploads', express.static(path.join(root, 'uploads')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(root, 'dist/client/index.html'));
 });
 
 app.listen(port, () => console.log('Listening on port 8000...'));
