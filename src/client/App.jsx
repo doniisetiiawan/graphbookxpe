ff3052881dd1716ce0caabdc9264ca1c59ff32f0
Doni <donisetiawan.050@gmail.com>
2019-04-10 13:28:14 +0700

    Add avatar with style

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
+import '../../assets/css/style.css';
 
-export default function App() {
-  return (
-    <div>
-      Hello World!
-    </div>
-  );
+const posts = [{
+  id: 2,
+  text: 'Lorem ipsum',
+  user: {
+    avatar: '/uploads/avatar1.png',
+    username: 'Test User',
+  },
+},
+{
+  id: 1,
+  text: 'Lorem ipsum',
+  user: {
+    avatar: '/uploads/avatar2.png',
+    username: 'Test User 2',
+  },
+}];
+
+export default class App extends Component {
+  state = {
+    posts,
+  };
+
+  render() {
+    const { posts } = this.state;
+    return (
+      <div className="container">
+        <div className="feed">
+          {posts.map(post => (
+            <div key={post.id} className="post">
+              <div className="header">
+                <img src={post.user.avatar} alt="avatar" />
+                <h2>{post.user.username}</h2>
+              </div>
+              <p className="content">
+                {post.text}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
