4c943e0ca2950e1705e478f8b772de0326f39abf
Doni <donisetiawan.050@gmail.com>
2019-04-11 09:27:04 +0700

    Add fake data

--- src/server/services/graphql/resolvers.js
+++ src/server/services/graphql/resolvers.js
@@ -1,8 +1,26 @@
 /* eslint-disable no-unused-vars */
+
+const posts = [{
+  id: 2,
+  text: 'Lorem ipsum',
+  user: {
+    avatar: '/uploads/avatar1.png',
+    username: 'Test User',
+  },
+},
+{
+  id: 1,
+  text: 'Lorem ipsum',
+  user: {
+    avatar: '/uploads/avatar2.png',
+    username: 'Test User 2',
+  },
+}];
+
 const resolvers = {
   RootQuery: {
     posts(root, args, context) {
-      return [];
+      return posts;
     },
   },
 };
