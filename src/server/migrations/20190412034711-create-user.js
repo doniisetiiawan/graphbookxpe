d6899816268b820256e689d7823dd744399d8f9a
Doni <donisetiawan.050@gmail.com>
2019-04-12 10:54:11 +0700

    Add relationship

---
+++ src/server/migrations/20190412034711-create-user.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-unused-vars */
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Users', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    avatar: {
+      type: Sequelize.STRING,
+    },
+    username: {
+      type: Sequelize.STRING,
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+  }),
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('Users'),
+};
