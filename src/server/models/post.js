6d5340143e7a1d9ea3325f578371468f4bde6e09
Doni <donisetiawan.050@gmail.com>
2019-04-12 11:51:42 +0700

    Add model association

--- src/server/models/post.js
+++ src/server/models/post.js
@@ -1,9 +1,10 @@
 module.exports = (sequelize, DataTypes) => {
   const Post = sequelize.define('Post', {
     text: DataTypes.TEXT,
+    userId: DataTypes.INTEGER,
   }, {});
   Post.associate = function (models) {
-    // associations can be defined here
+    Post.belongsTo(models.User);
   };
   return Post;
 };
