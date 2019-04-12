2eb38173a60842ba8ff3981ce1dbdd0dca0369fe
Doni <donisetiawan.050@gmail.com>
2019-04-12 17:11:07 +0700

    Add chat model and migration

---
+++ src/server/migrations/20190412072707-create-chat.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Chats', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+  }),
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('Chats'),
+};
---
+++ src/server/models/chat.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+module.exports = (sequelize, DataTypes) => {
+  const Chat = sequelize.define('Chat', {}, {});
+  Chat.associate = function (models) {
+    Chat.belongsToMany(models.User, { through: 'users_chats' });
+  };
+  return Chat;
+};
