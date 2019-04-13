4b6c2cfe947252b040d566e4e4f0e6ccf8648bbd
Doni <donisetiawan.050@gmail.com>
2019-04-13 14:33:47 +0700

    Add apollo query mutation

--- src/client/Feed.jsx
+++ src/client/Feed.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
-import { graphql } from 'react-apollo';
+import { Query } from 'react-apollo';
 
 const GET_POSTS = gql`{
   posts {
@@ -13,7 +13,7 @@ const GET_POSTS = gql`{
   }
 }`;
 
-class Feed extends Component {
+export default class Feed extends Component {
   state = {
     postContent: '',
   };
@@ -27,48 +27,44 @@ class Feed extends Component {
   };
 
   render() {
-    // eslint-disable-next-line react/prop-types
-    const { posts, loading, error } = this.props;
     const { postContent } = this.state;
-
-    if (loading) {
-      return 'Loading...';
-    }
-    if (error) {
-      return error.message;
-    }
-
     return (
       <div className="container">
         <div className="postForm">
           <form onSubmit={this.handleSubmit}>
-            <textarea value={postContent} onChange={this.handlePostContentChange} placeholder="Write your custom post!" />
+            <textarea
+              value={postContent}
+              onChange={this.handlePostContentChange}
+              placeholder="Write your custom post!"
+            />
             <input type="submit" value="Submit" />
           </form>
         </div>
         <div className="feed">
-          {/* eslint-disable-next-line no-unused-vars */}
-          {posts.map((post, i) => (
-            <div key={post.id} className="post">
-              <div className="header">
-                <img src={post.user.avatar} alt="avatar" />
-                <h2>{post.user.username}</h2>
-              </div>
-              <p className="content">
-                {post.text}
-              </p>
-            </div>
-          ))}
+          <Query
+            query={GET_POSTS}
+          >
+            {({ loading, error, data }) => {
+              if (loading) return 'Loading...';
+              if (error) return error.message;
+
+              const { posts } = data;
+
+              return posts.map(post => (
+                <div key={post.id} className="post">
+                  <div className="header">
+                    <img src={post.user.avatar} alt="avatar" />
+                    <h2>{post.user.username}</h2>
+                  </div>
+                  <p className="content">
+                    {post.text}
+                  </p>
+                </div>
+              ));
+            }}
+          </Query>
         </div>
       </div>
     );
   }
 }
-
-export default graphql(GET_POSTS, {
-  props: ({ data: { loading, error, posts } }) => ({
-    loading,
-    posts,
-    error,
-  }),
-})(Feed);
