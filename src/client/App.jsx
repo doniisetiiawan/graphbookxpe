b0e365272eb8c4d39c1ceb37b65dfd1d9f53bb8d
Doni <donisetiawan.050@gmail.com>
2019-04-13 12:36:17 +0700

    Using apollo client

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,84 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Helmet } from 'react-helmet';
+import Feed from './Feed';
 
 import '../../assets/css/style.css';
 
-const posts = [{
-  id: 2,
-  text: 'Lorem ipsum',
-  user: {
-    avatar: '/uploads/avatar1.png',
-    username: 'Test User',
-  },
-},
-{
-  id: 1,
-  text: 'Lorem ipsum',
-  user: {
-    avatar: '/uploads/avatar2.png',
-    username: 'Test User 2',
-  },
-}];
-
-export default class App extends Component {
-  state = {
-    posts,
-    postContent: '',
-  };
-
-  handlePostContentChange = (event) => {
-    this.setState({ postContent: event.target.value });
-  };
-
-  handleSubmit = (event) => {
-    const { posts, postContent } = this.state;
-    event.preventDefault();
-    const newPost = {
-      id: posts.length + 1,
-      text: postContent,
-      user: {
-        avatar: '/uploads/avatar1.png',
-        username: 'Fake User',
-      },
-    };
-    this.setState(prevState => ({
-      posts: [newPost, ...prevState.posts],
-      postContent: '',
-    }));
-  };
-
-  render() {
-    const { posts, postContent } = this.state;
-    return (
-      <div className="container">
-        <Helmet>
-          <title>Graphbook - Feed</title>
-          <meta name="description" content="Newsfeed  of all your friends on Graphbook" />
-        </Helmet>
-        <div className="postForm">
-          <form onSubmit={this.handleSubmit}>
-            <textarea
-              value={postContent}
-              onChange={this.handlePostContentChange}
-              placeholder="Write your custom post.."
-            />
-            <input type="submit" value="submit" />
-          </form>
-        </div>
-        <div className="feed">
-          {posts.map(post => (
-            <div key={post.id} className="post">
-              <div className="header">
-                <img src={post.user.avatar} alt="avatar" />
-                <h2>{post.user.username}</h2>
-              </div>
-              <p className="content">
-                {post.text}
-              </p>
-            </div>
-          ))}
-        </div>
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div className="container">
+      <Helmet>
+        <title>Graphbook - Feed</title>
+        <meta
+          name="description"
+          content="Newsfeed of all your friends on Graphbook"
+        />
+      </Helmet>
+      <Feed />
+    </div>
+  );
 }
